5ddbd746e131dbda9be48c92b9e640f55136aeba
wonyeonglee <iwonyeong22@gmail.com>
2021-05-12 11:20:00 +0900

    greedy1 문제 풀고 답 확인

--- greedyPractice1.py
+++ greedyPractice1.py
@@ -5,7 +5,6 @@ N = int(input())
 coin_sum = 0
 
 for change in changes:
-    if(N>=change):
-        coin_sum += N//change
-        N = N % change
+    coin_sum += N//change
+    N = N % change
 print(coin_sum)
